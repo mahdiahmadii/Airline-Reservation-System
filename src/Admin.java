a49acaaee204ec351570c4028ef869bd2a2ca328
MahdiAhmadi <119660029+mahdiahmadii@users.noreply.github.com>
2023-04-12 16:37:00 +0330

    Add files via upload

---
+++ src/Admin.java
@@ -0,0 +1,27 @@
+public class Admin {
+    /**
+     * features
+     */
+    private String name = "admin";
+    private String password = "admin";
+//____________________________________________________________
+
+    /**
+     *Getter & Setter
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
